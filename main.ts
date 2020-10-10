a7a79a68deda4ddb33c08c0d97b48248371cd26e
Rodrigoabrin <70342493+Rodrigoabrin@users.noreply.github.com>
2020-10-10 15:54:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+for (let index = 0; index < 4; index++) {
+    basic.showArrow(ArrowNames.North)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showArrow(ArrowNames.East)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showArrow(ArrowNames.West)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(100)
+}
+basic.forever(function () {
+	
+})
